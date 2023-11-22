5fe421eade1d5b97a188f3b6a1cb21b79f7fd385
liliane <cpaixaoliliane@gmail.com>
2023-11-22 06:54:55 -0300

    Testes Automatizados

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,3 @@
-const { hash, compare } = require("bcryptjs")
-const AppError = require("../utils/AppError")
-const sqliteConnection = require("../database/sqlite")
-
 //dentro de uma classe pode haver várias funções
 //No controller = regra do negócio
 
@@ -13,28 +9,24 @@ create - POST para criar um registro
 update - PUT para atualizar um registro
 delete - DELETE para remover um registro
 */
+const { hash, compare } = require("bcryptjs")
+const AppError = require("../utils/AppError")
+
+const UserRepository = require("../repositories/UserRepository")
+const sqliteConnection = require("../database/sqlite")
+const UserCreateService = require("../services/UserCreateService")
+
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
-    const database = await sqliteConnection()
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
-
-    if (checkUserExists) {
-      throw new AppError("Este e-mail já está em uso.")
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-    [name, email, hashedPassword]
-    )
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
+    await userCreateService.execute({ name, email, password })
 
     return response.status(201).json()
-  
-  }
+    }
 
   async update(request, response){
     const { name, email, password, old_password } = request.body
@@ -87,9 +79,6 @@ class UsersController {
   }
 
 }
-
-
-
 
 //exportar a classe
 module.exports = UsersController
